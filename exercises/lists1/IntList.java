30896974ba57cf15c6cfb2f5c253b3289f28c163
Bowen Zhang <bowenzhang1029@foxmail.com>
2022-11-22 14:58:52 +0800

    list1 exercises completed

--- exercises/lists1/IntList.java
+++ exercises/lists1/IntList.java
@@ -9,17 +9,29 @@ public class IntList {
 
 	/** Return the size of the list using... recursion! */
 	public int size() {
-		return 0;
+		if (this.rest == null)
+			return 0;
+		else
+			return 1 + this.rest.size();
 	}
 
 	/** Return the size of the list using no recursion! */
 	public int iterativeSize() {
-		return 0;
+		IntList temp = this;
+		int size = 0;
+		while (temp != null) {
+			size++;
+			temp = temp.rest;
+		}
+		return size;
 	}
 
 	/** Returns the ith value in this list.*/
 	public int get(int i) {
-		return 0;
+		if (i == 0)
+			return this.first;
+		else
+			return this.rest.get(i - 1);
 	}
 
 	public static void main(String[] args) {
@@ -27,6 +39,8 @@ public class IntList {
 		L = new IntList(10, L);
 		L = new IntList(5, L);
 
+		System.out.println(L.size());
 		System.out.println(L.iterativeSize());
+		System.out.println(L.get(2));
 	}
 } 
--- exercises/lists1/Lists1Exercises.java
+++ exercises/lists1/Lists1Exercises.java
@@ -1,33 +1,79 @@
-public class Lists1Exercises {
-    /** Returns an IntList identical to L, but with
-      * each element incremented by x. L is not allowed
-      * to change. */
-    public static IntList incrList(IntList L, int x) {
-        /* Your code here. */
-        return L;        
-    }
+public class IntList {
+	public int first;
+	public IntList rest;
+
+	public IntList(int f, IntList r) {
+		first = f;
+		rest = r;
+	}
+
+	/** Return the size of the list using... recursion! */
+	public int size() {
+		if (this.rest == null)
+			return 0;
+		else
+			return 1 + this.rest.size();
+	}
+
+	/** Return the size of the list using no recursion! */
+	public int iterativeSize() {
+		IntList temp = this;
+		int size = 0;
+		while (temp != null) {
+			size++;
+			temp = temp.rest;
+		}
+		return size;
+	}
+
+	/** Returns the ith value in this list.*/
+	public int get(int i) {
+		if (i == 0)
+			return this.first;
+		else
+			return this.rest.get(i - 1);
+	}
 
     /** Returns an IntList identical to L, but with
-      * each element incremented by x. Not allowed to use
-      * the 'new' keyword. */
-    public static IntList dincrList(IntList L, int x) {
-        /* Your code here. */
-        return L;
-    }
+     * each element incremented by x. L is not allowed
+     * to change. */
+   public static IntList incrList(IntList L, int x) {
+       L = new IntList(L.first + x, L.rest);
+       IntList J = L;
+       while (J.rest != null) {
+    	   J.rest = new IntList(J.rest.first + x, J.rest.rest);
+    	   J = J.rest;    	   
+       }
+       return L;        
+   }
 
-    public static void main(String[] args) {
-        IntList L = new IntList(5, null);
-        L.rest = new IntList(7, null);
-        L.rest.rest = new IntList(9, null);
+   /** Returns an IntList identical to L, but with
+     * each element incremented by x. Not allowed to use
+     * the 'new' keyword. */
+   public static IntList dincrList(IntList L, int x) {
+       IntList J = L;
+       while (J != null) {
+    	   J.first += x;
+    	   J = J.rest;
+       }
+       return L;
+   }
 
-        System.out.println(L.size());
-        System.out.println(L.iterativeSize());
+   public static void main(String[] args) {
+       IntList L = new IntList(5, null);
+       L.rest = new IntList(7, null);
+       L.rest.rest = new IntList(9, null);
 
-        // Test your answers by uncommenting. Or copy and paste the
-        // code for incrList and dincrList into IntList.java and
-        // run it in the visualizer.
-        // System.out.println(L.get(1));
-        // System.out.println(incrList(L, 3));
-        // System.out.println(dincrList(L, 3));        
-    }
-}
+       System.out.println(L.size());
+       System.out.println(L.iterativeSize());
+
+       // Test your answers by uncommenting. Or copy and paste the
+       // code for incrList and dincrList into IntList.java and
+       // run it in the visualizer.
+
+       System.out.println(incrList(L, 3).get(2));
+       System.out.println(L.get(2));
+       System.out.println(dincrList(L, 3).get(2));
+       System.out.println(L.get(2));
+   }
+} 
